787ff145b246e506beb1c6aae404f25950990dbb
Hsiehmengtung <162270355+Hsiehmengtung@users.noreply.github.com>
2024-06-10 14:05:44 +0800

    Update cust_login_check.php

--- 資料庫final project/cust_login_check.php
+++ 資料庫final project/cust_login_check.php
@@ -12,18 +12,25 @@ if (isset($_GET['submit'])){
 }
 
 function checkData($cust_name, $cust_pw, $conn) {
-  $sql = "SELECT cust_name, cust_pw FROM customer WHERE cust_name = '$cust_name' AND cust_pw = '$cust_pw'";
+  $sql = "SELECT cust_id, cust_name FROM customer WHERE cust_name = '$cust_name'";
   $result = mysqli_query($conn, $sql);
   if(mysqli_num_rows($result) == 0) {
-    echo "帳號或密碼錯誤";
-    header("Location: ./login.php?error=帳號或密碼錯誤");   
-  } else {
+  	header("Location: ./login.php?error=no_cust_name");   
+  } 
+    else{
+    $sql = "SELECT cust_id, cust_name FROM customer WHERE cust_name = '$cust_name' AND cust_pw = '$cust_pw'";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) == 0) {
+      header("Location: ./login.php?error=wrong_cust_pwd");   
+    }
+    else {
     $row = mysqli_fetch_assoc($result);
     echo "登入成功";
     $_SESSION['cust_name'] = $row['cust_name'];
     $_SESSION['cust_pw'] = $row['cust_pw'];
-    header("Location: ./home.php");  //理論上要連到客戶主頁，但還沒建就先連這
-  }
+    header("Location: ./home.php");
+    }
+    }
 }
 $conn->close();
 ?>
