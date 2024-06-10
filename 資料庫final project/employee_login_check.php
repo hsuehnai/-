b34177142b000a80b226bb35808c69e20ded9545
Hsiehmengtung <162270355+Hsiehmengtung@users.noreply.github.com>
2024-06-10 14:02:06 +0800

    Update employee_login_check.php

--- 資料庫final project/employee_login_check.php
+++ 資料庫final project/employee_login_check.php
@@ -6,26 +6,32 @@ if (isset($_GET['submit'])){
   $query = [
     'employee_name' => htmlspecialchars($_GET["employee_name"]),
     'employee_pw' => htmlspecialchars($_GET["employee_pw"]),
-    'employee_store' => htmlspecialchars($_GET["employee_store"]),
   ];
   $conn = db_check();
-  checkData($query['employee_name'], ($query['employee_pw']), ($query['employee_store']), $conn);
+  checkData($query['employee_name'], ($query['employee_pw']), $conn);
 }
 
-function checkData($employee_name, $employee_pw, $employee_store, $conn) {
-  $sql = "SELECT employee_name, employee_pw, employee_store FROM employee WHERE employee_name = '$employee_name' AND employee_pw = '$employee_pw' AND employee_store = '$employee_store'";
+function checkData($employee_name, $employee_pw, $conn) {
+  $sql = "SELECT employee_id, employee_name FROM employee WHERE employee_name = '$employee_name'";
   $result = mysqli_query($conn, $sql);
   if(mysqli_num_rows($result) == 0) {
-    echo "帳號或密碼錯誤";
-    header("Location: ./login.php?error=帳號或密碼錯誤");   
-  } else {
+    header("Location: ./login.php?error=no_employee_name");   
+  } 
+    else{
+    $sql = "SELECT employee_id, employee_name FROM employee WHERE employee_name = '$employee_name' AND employee_pw = '$employee_pw'";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) == 0) {
+      header("Location: ./login.php?error=wrong_employee_pw");   
+    }
+    else {
     $row = mysqli_fetch_assoc($result);
     echo "登入成功";
     $_SESSION['employee_name'] = $row['employee_name'];
     $_SESSION['employee_pw'] = $row['employee_pw'];
     $_SESSION['employee_store'] = $row['employee_store'];
     header("Location: ./home.php");
-  }
+    }
+    }
 }
 $conn->close();
 ?>
