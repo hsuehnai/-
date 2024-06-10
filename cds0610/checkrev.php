9250a5f81163b80ea5204f64d675006819dd6419
cds183 <162258837+cds183@users.noreply.github.com>
2024-06-10 14:51:35 +0800

    Add files via upload

---
+++ cds0610/checkrev.php
@@ -0,0 +1,34 @@
+<?php
+require_once dirname(__FILE__)."/db_check.php";
+session_start();
+$conn = db_check();
+
+storerev($conn);
+
+function storerev($conn) {
+  $e_store = $_SESSION['employee_store'];
+  $s_num = "SELECT store_name, store_chic, store_fish, store_pork, store_beef, store_num, store_revenue FROM store WHERE store_id = '$e_store'";
+  $result = mysqli_query($conn, $s_num);
+
+  if(mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    $sname = $row["store_name"];
+    $store_chic = $row['store_chic'];
+    $store_fish = $row['store_fish'];
+    $store_pork = $row['store_pork'];
+    $store_beef = $row['store_beef'];
+    $snum = $row['store_num'];
+    $srev = $row['store_revenue'];
+  }
+
+  echo "<h1>$sname 營業額</h1>" . "<br>";
+  echo "照燒雞胸賣出數量：" . $store_chic . "<br>";
+  echo "薄鹽鯖魚賣出數量：" . $store_fish . "<br>";
+  echo "泡菜豬里肌賣出數量：" . $store_pork . "<br>";
+  echo "日式壽喜牛賣出數量：" . $store_beef . "<br>";
+  echo $sname . "賣出餐盒總數量：" . $snum . "<br>";
+  echo "<b>=></b> " . $sname . "總營業額：" . $srev . "<br>";
+}
+
+$conn->close();
+?>
---
+++ cds0610/home.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+	<head>
+        <meta charset="utf-8">
+        <title>員工主頁 - EHOMEPAGE</title>
+	</head>
+	<?php
+        require_once dirname(__FILE__)."/db_check.php";
+		session_start();
+        $conn = db_check();
+		echo "Hello,  " . $_SESSION['employee_name'] . "! 😄" . "<br>";
+        
+        $e_store = $_SESSION['employee_store'];
+        $sname = "SELECT store_name FROM store WHERE store_id = '$e_store'";
+        $sresult = mysqli_query($conn, $sname);
+        if(mysqli_num_rows($sresult) > 0) {
+            $row = $sresult->fetch_assoc();
+            $sname = $row["store_name"];
+        }
+        echo "分店：" . $sname;
+
+        $conn->close();
+	    ?>
+
+    <body> 
+        <button type="button" id="B5" onclick="window.location ='./employee_login.php'">登出 <br> Logout</button> <!--檔案名稱改成登入頁面-->
+	<div class="container">
+         
+        <img src="雞胸餐盒.jpg" alt="Logo">
+		
+		<!--<button type="button" id="B1" onclick="window.location ='./custreg.php'">新增會員（顧客）資料 <br> Add New Customer</button>-->
+		<button type="button" id="B2" onclick="window.location ='./order.php'">輸入訂單 <br> Place order</button>
+		<button type="button" id="B3" onclick="window.location ='./getestore.php'">查看分店訂單 <br> Check store's order</button>
+		<button type="button" id="B4" onclick="window.location ='./checkrev.php'">查看分店營業額 <br> Store's Revenue</button>
+	</div>
+	</body>
+	<style>
+	body {
+                background-color: #FFFFE0;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                margin: 0;
+                font-family: Arial, sans-serif;
+            }
+
+            .container {
+                text-align: center;
+            }
+
+            button {
+                display: block;
+                width: 200px;
+                margin: 10px auto;
+                padding: 10px;
+                font-size: 16px;
+                cursor: pointer;
+                border: 1px solid #ccc;
+                border-radius: 5px;
+                background-color: #f0f0f0;
+                transition: background-color 0.3s;
+            }
+
+            button:hover {
+                background-color: #ddd;
+            }
+			img {
+                width: 300px; /* Adjust the width as needed */
+                height: auto; /* Maintain the aspect ratio */
+                margin: 20px 0; /* Space between image and text/buttons */
+				border-radius: 50%;
+            }
+			
+	</style>
+</html>
